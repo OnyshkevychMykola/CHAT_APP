e7fd03332b858521f24c1bee4dd7b7e7015a54f7
Mykola Onyshkevych <mykola.onyshkevych@vitechteam.com>
2023-11-13 22:03:21 +0200

    updated message update (front part)

--- chat-app-client/src/app/main/components/chat-message/chat-message.component.ts
+++ chat-app-client/src/app/main/components/chat-message/chat-message.component.ts
@@ -1,29 +1,38 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { MessageI } from 'src/app/model/message.interface';
 import { UserI } from 'src/app/model/user.interface';
 import { AuthService } from 'src/app/auth/services/auth-service/auth.service';
 import {RoomI} from "../../../model/room.interface";
 import {ChatService} from "../../services/chat.service";
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-chat-message',
   templateUrl: './chat-message.component.html',
   styleUrls: ['./chat-message.component.scss']
 })
-export class ChatMessageComponent {
+export class ChatMessageComponent implements OnInit{
 
   @Input() message: MessageI;
   @Input() chatRoom: RoomI;
   user: UserI = this.authService.getLoggedInUser();
   edit: boolean = false;
+  messageText: FormControl = new FormControl(null, [Validators.required]);
+
+  ngOnInit() {
+  }
 
   constructor(private authService: AuthService, private chatService: ChatService) { }
 
   changeEdit() {
+    if (this.message.user.id !== this.user.id) return;
     this.edit = !this.edit
   }
 
   updateMessage() {
-    this.chatService.updateMessage( { text: 'newText', roomId: this.chatRoom.id, messageId: this.message.id });
+    if (!this.messageText.value) return
+    this.chatService.updateMessage( { text: this.messageText.value, roomId: this.chatRoom.id, messageId: this.message.id });
+    this.message.text = this.messageText.value;
+    this.edit = !this.edit
   }
 }
